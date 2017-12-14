f9cfe4851c508b0eb446fe77e6a112198cf411d9
13048892422@163.com <13048892422@163.com>
2017-12-14 15:26:06 +0800

    update index2.js

--- 20171213/index2.js
+++ 20171213/index2.js
@@ -1,7 +1,17 @@
-const s  = {name:"xxx"}
-
-console.error(s.name);
-
-s.name = "xcz";
-
-s = {};    
+/**
+ * typeof  返回数据类型
+ *JavaScript共有七种数据类型
+ *  primitive  基本数据类型
+ *     --boolean  true or false
+ *     --null 一个表示null值的特殊的关键字
+ *     --undefined 变量未定义时的属性
+ *     --number 表示数字 8字节 NaN是number类型
+ *     --String 表示字符串
+ *     --Symbol ( 在 ECMAScript 6 中新添加的类型).。一种数据类型，它的实例是唯一且不可改变的。
+ * object 对象数据类型
+ *      objects and functions 是本语言的两个基本要素，
+ *      可以将object视为存放值的容器，而function是你的应用程序能执行的过程
+ * 
+ * false 等效值 null,undefined,"",0,NaN
+ * 
+ */
